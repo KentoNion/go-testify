acdef822e2d443fb284056254eff08b94dda5fba
KentoNion <mrkentonion@gmail.com>
2024-09-02 00:34:49 +0300

    есть Ðнесколько вÑопросов, во первых я не понимаю почему у меня все тесты феÐйлятся на несоответсвии кодов, все тÐесты вÐыдают код 400 вместо 200, при этом код самой программы в precode.go я взял  из задания и не менял его, во 2 я не понимаÑÐю почему asser.Len(t, strings.Splitю . . . и тд выдаёт отшибку что мол в боди 11 элементов, а Ðожидается 100, отдÐуда он взял 11? их же должно быть 4?

--- precode_test.go
+++ precode_test.go
@@ -5,15 +5,9 @@ import (
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
-
-// не тот город
-//func TestWrongCity(t *testing.T) {
-//	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
-//	responseRecorder := httptest.NewRecorder()
-
-//}
 
 // проверка на непустое тело и статус ОК
 func TestBodyNotEmpty(t *testing.T) {
@@ -21,9 +15,10 @@ func TestBodyNotEmpty(t *testing.T) {
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
-	body := responseRecorder.Body.String()
-	require.NotEmpty(t, body)                              //непустое тело
+
+	body := responseRecorder.Body
 	require.Equal(t, http.StatusOK, responseRecorder.Code) //Статус ОК
+	assert.NotEmpty(t, body)                               //непустое тело
 }
 
 // не тот город
@@ -32,17 +27,20 @@ func TestWrongCity(t *testing.T) {
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
+
 	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
-	body := responseRecorder.Body.String()
+	body := responseRecorder.Body
 	assert.NotEmpty(t, body)
 }
 
+// Счётчик больше максимума
 func TestCounterMoreThanMax(t *testing.T) {
 	req := httptest.NewRequest("GET", "/cafe?count=100?city=jopa", nil)
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
+
 	body := responseRecorder.Body.String()
-	assert.NotEmpty(t, body)
 	require.Equal(t, http.StatusOK, responseRecorder.Code)
+	assert.Len(t, strings.Split(body, ","), 100)
 }
